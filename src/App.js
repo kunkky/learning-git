e48bcad299520c2059f14a20fc78965515cd54f9
kunkky <kunkkykukky@gmail.com>
2022-05-15 22:37:44 -1200

    hello added to App line 6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="row">
+        <div class="col-6 col-sm-3">Hello</div>
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+
+        <div class="w-100"></div>
+
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+      </div>
     </div>
-  );
+      );
 }
 
 export default App;
