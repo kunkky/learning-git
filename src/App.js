ad260512b9299dd43ff4625ae946a92880ddde0a
kunkky <83945048+kunkky@users.noreply.github.com>
2022-05-15 22:58:26 -1200

    line 7 ofthe appjs was edited again
    just learning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     <div class="container">
       <div class="row">
         <div class="col-6 col-sm-3">Hello</div>
-        <div class="col-6 col-sm-3">.col-6 .col-sm-3</div>
+        <div class="col-6 col-sm-3">World</div>
 
         <div class="w-100"></div>
 
